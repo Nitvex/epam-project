9a625f84fdf37e7632586b7d081803d6fbc510a7
Nitvex <vozisovnikita@gmail.com>
2018-10-19 00:15:41 +0300

    [Add] routing

--- src/client/index.js
+++ src/client/index.js
@@ -2,6 +2,19 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './components/main/App';
+import About from './components/about/About';
+import Account from './components/account/Account';
+import Login from './components/login/Login';
+import {HashRouter as Router, Route, Switch} from "react-router-dom";
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(
+    <Router>
+        <Switch>
+            <Route exact path="/" component={App}/>
+            <Route path="/about" component={About}/>
+            <Route path="/account" component={Account}/>
+            <Route path="/login" component={Login}/>
+        </Switch>
+    </Router>,
+    document.getElementById('root'));
 
