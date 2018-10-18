ad65e2aad75a41fdcfaf8edbb2ceb6d543d53585
Nitvex <vozisovnikita@gmail.com>
2018-10-19 00:10:49 +0300

    [Add] initial commit for pages

---
+++ src/client/components/account/Account.js
@@ -0,0 +1,14 @@
+import React, {Component} from 'react';
+
+
+class About extends Component {
+    render() {
+        return (
+            <div>
+                Account
+            </div>
+        );
+    }
+}
+
+export default About;
