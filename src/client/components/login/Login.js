ba945fa2f6836ab3386d23b8f0e0e50ca45053d4
Nitvex <vozisovnikita@gmail.com>
2018-10-19 01:47:02 +0300

    [Add] "Authentication" button

--- src/client/components/login/Login.js
+++ src/client/components/login/Login.js
@@ -1,7 +1,32 @@
 import React, {Component} from 'react';
 import {Link} from "react-router-dom";
+import {authenticate} from "../../actions/authenticate";
+import {logout} from "../../actions/logout";
+import {connect} from 'react-redux';
 
-class Login extends Component {
+const mapStateToProps = state => {
+    return {isAuthenticated: state.isAuthenticated};
+};
+const mapDispatchToProps = dispatch => {
+    return {
+        authenticate: () => dispatch(authenticate()),
+        logout: () => dispatch(logout())
+    };
+};
+
+class connectedLogin extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    handleClick = () => {
+        console.log(this.props.isAuthenticated);
+        this.props.isAuthenticated ?
+            this.props.logout() :
+            this.props.authenticate();
+    };
+
     render() {
         return (
             <div>
@@ -15,9 +40,16 @@ class Login extends Component {
                 <br/>
                 <Link to="/login">login</Link>
                 <br/>
+                {this.props.isAuthenticated ?
+                    <button onClick={this.handleClick}>Sign out</button> :
+                    <button onClick={this.handleClick}>Sign in</button>
+                }
             </div>
         );
     }
 }
+
+
+const Login = connect(mapStateToProps, mapDispatchToProps)(connectedLogin);
 
 export default Login;
