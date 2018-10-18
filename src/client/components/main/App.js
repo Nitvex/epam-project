7de0d5e8e0b5f2e7411cb93c1a9a1e536bf26896
Nitvex <vozisovnikita@gmail.com>
2018-10-19 00:18:02 +0300

    [Add] Links to other pages from App

--- src/client/components/main/App.js
+++ src/client/components/main/App.js
@@ -1,15 +1,22 @@
-import React, { Component } from 'react';
-
+import React, {Component} from 'react';
+import {Link} from "react-router-dom";
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-        Hello App
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                Hello App
+                <br/>
+                <Link to="/account">Account</Link>
+                <br/>
+                <Link to="/about">About</Link>
+                <br/>
+                <Link to="/login">Login</Link>
+                <br/>
+            </div>
+        );
+    }
 }
 
 export default App;
