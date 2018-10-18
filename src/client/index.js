14bb5f4d5a49f1951523184d2e8056f82923f454
Nitvex <vozisovnikita@gmail.com>
2018-10-19 00:34:35 +0300

    [Add] connected store to whole project

--- src/client/index.js
+++ src/client/index.js
@@ -6,15 +6,19 @@ import About from './components/about/About';
 import Account from './components/account/Account';
 import Login from './components/login/Login';
 import {HashRouter as Router, Route, Switch} from "react-router-dom";
+import {Provider} from "react-redux";
+import store from "./store/index";
 
 ReactDOM.render(
-    <Router>
-        <Switch>
-            <Route exact path="/" component={App}/>
-            <Route path="/about" component={About}/>
-            <Route path="/account" component={Account}/>
-            <Route path="/login" component={Login}/>
-        </Switch>
-    </Router>,
+    <Provider store={store}>
+        <Router>
+            <Switch>
+                <Route exact path="/" component={App}/>
+                <Route path="/about" component={About}/>
+                <Route path="/account" component={Account}/>
+                <Route path="/login" component={Login}/>
+            </Switch>
+        </Router>
+    </Provider>,
     document.getElementById('root'));
 
