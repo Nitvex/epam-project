50a106c5f8067ed809fe0735af8cbb3338ccf739
Nitvex <vozisovnikita@gmail.com>
2018-10-18 23:57:18 +0300

    Initial commit

---
+++ src/client/index.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+import './index.css';
+import App from './components/main/App';
+
+ReactDOM.render(<App />, document.getElementById('root'));
+
