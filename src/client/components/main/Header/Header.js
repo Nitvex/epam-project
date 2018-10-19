ac97678056cae5eb1691d55ca356428cbff508af
Nitvex <vozisovnikita@gmail.com>
2018-10-19 15:14:35 +0300

    [Add] header log(in|out)

--- src/client/components/main/Header/Header.js
+++ src/client/components/main/Header/Header.js
@@ -1,9 +1,29 @@
 import React, {Component} from 'react';
 import {Link} from "react-router-dom";
 import './style.css';
+import {connect} from "react-redux";
+import {logout} from "../../../actions/logout";
 
+const mapStateToProps = state => {
+    return {isAuthenticated: state.isAuthenticated};
+};
 
-class Header extends Component {
+const mapDispatchToProps = dispatch => {
+    return {
+        logout: () => dispatch(logout())
+    };
+};
+
+class connectedHeader extends Component {
+
+    constructor(props) {
+        super(props);
+    }
+
+    logout = () => {
+        this.props.logout();
+    };
+
     render() {
         return (
             <header className="header">
@@ -11,11 +31,16 @@ class Header extends Component {
                     <Link to="/" className="nav-item nav-link" href="#">Main page</Link>
                     <Link to="/about" className="nav-item nav-link" href="#">About</Link>
                     <Link to="/account" className="nav-item nav-link" href="#">Account</Link>
-                    <Link to="/login" className="nav-item nav-link" href="#">Login</Link>
+                    {
+                        this.props.isAuthenticated ?
+                            <Link exact to="/" onClick={this.logout} href="#" className="nav-item nav-link">Log out</Link> :
+                            <Link to="/login" className="nav-item nav-link" href="#">Login</Link>
+                    }
                 </nav>
             </header>
         );
     }
 }
 
+const Header = connect(mapStateToProps, mapDispatchToProps)(connectedHeader);
 export default Header;
