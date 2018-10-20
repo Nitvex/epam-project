e96aa37bf8f6e0992018c76a4b02e12c52467a66
Nitvex <vozisovnikita@gmail.com>
2018-10-20 14:59:08 +0300

    [Delete] redux-thunk

--- src/client/store/index.js
+++ src/client/store/index.js
@@ -3,7 +3,6 @@ import reducer from "../reducers/index";
 import {fetchMiddleware} from "../middleware/fetchMiddleware";
 import {authenticateMiddleware} from "../middleware/authenticateMiddleware";
 import {composeWithDevTools} from 'redux-devtools-extension';
-import thunk from 'redux-thunk';
 
-const store = createStore(reducer, composeWithDevTools(applyMiddleware(thunk, fetchMiddleware, authenticateMiddleware)));
+const store = createStore(reducer, composeWithDevTools(applyMiddleware(fetchMiddleware, authenticateMiddleware)));
 export default store;
