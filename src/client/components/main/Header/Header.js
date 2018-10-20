2bc68140e8828ff1c0efffa1bce7a79515ab9aba
Nitvex <vozisovnikita@gmail.com>
2018-10-21 01:25:08 +0300

    [Refactor] Header content is centered and black;log(in|out) is in right

--- src/client/components/main/Header/Header.js
+++ src/client/components/main/Header/Header.js
@@ -41,15 +41,15 @@ class connectedHeader extends Component {
     render() {
         return (
             <header className="header">
-                <nav className="nav nav-pills justify-content-end">
+                <nav className="nav nav-pills justify-content-center">
                     <Link to="/" className="nav-item nav-link" href="#">Main page</Link>
                     <Link to="/about" className="nav-item nav-link" href="#">About</Link>
                     <Link to="/account" className="nav-item nav-link" href="#">Account</Link>
                     {
                         ((localStorage.getItem('authenticated') === 'yes') || (this.props.isAuthenticated)) ?
-                            <Link exact to="/" onClick={this.logout} href="#" className="nav-item nav-link">Log
+                            <Link exact to="/" onClick={this.logout} href="#" className="nav-item nav-link logout">Log
                                 out</Link> :
-                            <Link to="/login" className="nav-item nav-link" href="#">Login</Link>
+                            <Link to="/login" className="nav-item nav-link logout" href="#">Login</Link>
                     }
                 </nav>
             </header>
