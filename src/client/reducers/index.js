4a371925f97bbda11b6a0a54a9452de55e69f236
Nitvex <vozisovnikita@gmail.com>
2018-10-20 15:27:04 +0300

    [Delete] react-router-redux

--- src/client/reducers/index.js
+++ src/client/reducers/index.js
@@ -4,10 +4,8 @@ import correctInputReducer from "./correctInput";
 import informationReducer from "./information";
 import locationsReducer from './locations';
 import recordsReducer from './records';
-import {routerReducer} from 'react-router-redux'
 
 export default combineReducers({
-    routing: routerReducer,
     authenticationReducer,
     correctInputReducer,
     informationReducer,
